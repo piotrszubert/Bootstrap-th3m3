3b8a0c2770f4dbec28c765b0539e84e61f39b13f
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-10-10 12:43:38 +0000

    added components page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import path from 'path'
+import { resolve } from 'path'
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        test: resolve(__dirname, 'src/components.html'),
+      },
+    },
   },
   server: {
     port: 8080
