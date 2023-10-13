3c3b48182e7a9b34ab9a1dcfb6c92603cdbd6182
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-10-13 07:09:08 +0000

    added aos library

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,14 @@ import '../scss/styles.scss'
 
 import { Popover } from 'bootstrap';
 
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+AOS.init();
+
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new Popover(popover)
-  })
+})
